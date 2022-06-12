8b8d1c50ee8c10f6b8a0a12259ed121f14de5703
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2022-06-12 15:37:21 +0300

    Update Program.cs

--- CS1/Program.cs
+++ CS1/Program.cs
@@ -6,10 +6,9 @@ namespace CS1
     {
         static void Main(string[] args)
         {
-            
             byte health = 100;
             char stamina = Convert.ToChar(100); 
-            bool power = Convert.ToBoolean(80);
+            bool willPower = Convert.ToBoolean(80);
             long mana = 90;
             sbyte attack = 25;
             short protection = 15;
