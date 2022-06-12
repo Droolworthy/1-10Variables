0af6ca68f05b35d3dce020667d3837c3ddf3728f
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2022-06-12 15:03:24 +0300

    Добавьте файлы проекта.

---
+++ CS1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CS1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            byte health = 100;
+            char stamina = Convert.ToChar(100); 
+            bool power = Convert.ToBoolean(80);
+            long mana = 90;
+            sbyte attack = 25;
+            short protection = 15;
+            int intellect = 45;
+            ulong agility = 60;  
+            ushort speed = 75;   
+            string flexibility = Convert.ToString(20);
+        }
+    }
+}
